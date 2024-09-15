3f66a91f28e26c0ce12149a4b44b42f00dd20cec
James Harris <jamesharris126@gmail.com>
2024-09-14 20:36:44 -0500

    first commit

---
+++ Homework1.py
@@ -0,0 +1,43 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_con(hostname, username, userpw, dbname):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host = hostname,
+            user = username,
+            password = 'UHcougars28',
+            database = dbname
+        )
+        print("connection successfull")
+
+    except Error as e:
+        print(f'the error {e} occured')
+    return connection
+
+conn = create_con('cis2368fall.cfe4cqq8sj4h.us-east-1.rds.amazonaws.com', 'admin', 'cis2368fall', 
+                   'cis2368falldb')
+
+cursor = conn.cursor(dictionary = True)
+
+sql = "SELECT * FROM menu"
+
+
+def main():
+
+    option = int(input('Welcome to the Cougar Cafe!!!!!!\n'
+              'Please select from the options below:\n'
+              'Press 1 to view our full menu\n'
+              'Press 2 to start an order\n'
+              'Press 3 to exit'))
+    
+    if option == 1:
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+
+        for x in rows:
+            print(x)
+
+if __name__ == "__main__":
+    main()
